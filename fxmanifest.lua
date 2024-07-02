2ae26be52245eaea14704d1ca50357b88cb92e21
! DevJujuri <zuhrihazim1970@gmail.com>
2024-07-03 03:11:24 +0800

    Update Fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'MrJujuri#8654'
+description 'A resource to send FiveM server ping to a Node.js server for atlassian statuspage'
+version '1.0.0'
+
+server_script 'uptime.lua'
